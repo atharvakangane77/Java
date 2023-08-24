6719450b03073855c79375c3719e577f7ef495d8
atharvakangane77 <128119458+atharvakangane77@users.noreply.github.com>
2023-08-24 19:44:53 +0530

    by git

--- GITdemo/src/Demo.java
+++ GITdemo/src/Demo.java
@@ -3,6 +3,9 @@ public class Demo {
 	public void m1() {
 		System.out.println("m1--demo");
 	}
+	public void m2(){
+		System.out.println("m2--demo");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello GIT");
 	}
